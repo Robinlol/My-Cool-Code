30850dfcfa299ac68ce44e98a1d3f6b8b1a310f0
Rob <robgtav1@gmail.com>
2020-04-27 21:52:47 +0200

    NestedLoop

--- NestedLoops.js
+++ NestedLoops.js
@@ -5,8 +5,18 @@ createCanvas(600, 400);
 }
 
 function draw() {
-backround(0);
-rect(100,110, 50, 50);
+background(0);
+
+for(var x = 0; x<= width; x+=50){
+  for(var y = 0; y<= width; y+=50){
+    fill(random(255), 0, random(255));
+    ellipse(x,y,25,25);
+
+    }
+
+
+
+}
 
 
 
