667a3033bbc926046b75d363f71d7671d4f93cd1
Rob <robgtav1@gmail.com>
2020-04-30 19:27:19 +0200

    added Moon

--- Functions.js
+++ Functions.js
@@ -3,8 +3,9 @@ function setup() {
 createCanvas(600, 400);
 var kgEarth =55;
 var kgMars=conversion(kgEarth);
-print('Mars '+kgMars);
-
+var kgMoon=conversion2(kgEarth);
+print('Mars '+kgMars+'kg');
+print('Moon '+kgMoon+'kg');
 }
 
 
@@ -12,6 +13,11 @@ print('Mars '+kgMars);
 function conversion(kgEarth) {
   var kgMars =kgEarth /9.8 *3.711;
   return kgMars;
+}
+
+function conversion2(kgEarth) {
+  var kgMoon =kgEarth /9.8 *1.62;
+  return kgMoon;
 }
 
 
