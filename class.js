1e486d127dbbd3aa8d03dc2d70c9579a4b799ff5
Rob <robgtav1@gmail.com>
2020-05-03 00:06:21 +0200

    Function example

--- class.js
+++ class.js
@@ -1,30 +1,78 @@
 
-
+let cow;
+let dog;
 function setup() {
 
 createCanvas(800,400);  // put setup code here
+cow = new Box(400,200,20,0,0,0);
+dog = new Dog(300,100);
 
-let box;
-box= new Box();
-print(box.x,box.y);
+//print(box.x,box.y);
 
 }
 
 
-class Box{
-  constructor() {
 
-   this.x =1;
-   this.y = 1;
-
-
-  }
-}
 
 
 
 function draw() {
-  background(0);
+background(0);
+  cow.show();
+  cow.move();
+  dog.show2();
 
 
 }
+
+class Box{
+  constructor(x,y,r,red,o,b) {
+
+   this.x =x;
+   this.y =y;
+   this.r =r;
+   this.red =red;
+   this.o =o;
+   this.b= b;
+ }
+
+   move(){
+     this.x = this.x + random(-5,5);
+        this.y = this.y + random(-5,5);
+          this.red = this.red + 1;
+
+            this.o = this.o + 1;
+
+   }
+
+   show(){
+     fill(this.red,this.o,this.b);
+     ellipse(this.x, this.y, this.r*2);
+
+
+
+   }
+
+   }
+
+   class Dog {
+     constructor(x,y) {
+
+       this.x=x;
+       this.y=y;
+
+
+
+
+
+     }
+
+
+     show2(){
+       rect(this.x,this.y,100,100);
+
+
+
+
+     }
+   }
